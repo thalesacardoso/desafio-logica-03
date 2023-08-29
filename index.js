01725c8c3d48c564f23f27409bc67ab62b99ac54
thalesacardoso <thales.dev@hotmail.com>
2023-08-29 15:33:56 -0300

    Criação da classe e atributos

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Desafio de projeto 03
+
+class heroi {
+    constructor(nome, idade , tipo){
+        this.nomeHeroi = nome
+        this.idadeHeroi = idade
+        this.tipoHeroi = tipo
+        this.ataque
+    }
+
+    escrever(){
+        console.log("O nome do herói é " + this.nomeHeroi + " e tem " + this.idadeHeroi + " anos")
+    }
+
+}
+
+let hero = new heroi("Batman" , 20 , "Mago")
+
+console.log(hero.idadeHeroi)
